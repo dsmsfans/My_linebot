523d98dfef941df4d2d72945097ab9c2c08cf8b4
Casper <aaa40066@gmail.com>
2020-05-28 21:45:23 +0800

    test

--- app.py
+++ app.py
@@ -62,29 +62,29 @@ def handle_message(event):
         replay_message(event,message)
     elif ("測試" in msg):
         buttons_template = TemplateSendMessage(
-        alt_text='Buttons Template',
-        template=ButtonsTemplate(
-            title='這是ButtonsTemplate',
-            text='ButtonsTemplate可以傳送text,uri',
-            thumbnail_image_url='顯示在開頭的大圖片網址',
-            actions=[
-                MessageTemplateAction(
-                    label='ButtonsTemplate',
-                    text='ButtonsTemplate'
-                ),
-                URITemplateAction(
-                    label='VIDEO1',
-                    uri='影片網址'
-                ),
-                PostbackTemplateAction(
-                    label='postback',
-                    text='postback text',
-                    data='postback1'
-                )
-            ]
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+                title='這是ButtonsTemplate',
+                text='ButtonsTemplate可以傳送text,uri',
+                thumbnail_image_url='顯示在開頭的大圖片網址',
+                actions=[
+                    MessageTemplateAction(
+                        label='ButtonsTemplate',
+                        text='ButtonsTemplate'
+                    ),
+                    URITemplateAction(
+                        label='VIDEO1',
+                        uri='影片網址'
+                    ),
+                    PostbackTemplateAction(
+                        label='postback',
+                        text='postback text',
+                        data='postback1'
+                    )
+                ]
+            )
         )
-    )
-    line_bot_api.reply_message(event.reply_token, buttons_template)
+        line_bot_api.reply_message(event.reply_token, buttons_template)
     elif ("範例" in msg):     
         Confirm_template = TemplateSendMessage(
         alt_text='目錄 template',
