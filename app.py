8bb3d892004248ccd08c466876b5f749a9ebd52e
Casper <aaa40066@gmail.com>
2020-06-22 10:52:07 +0800

    template seperate

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError,LineBotApiError
+    InvalidSignatureError, LineBotApiError
 )
 from linebot.models import *
 import os
@@ -16,6 +16,8 @@ line_bot_api = LineBotApi(os.environ['lineToken'])
 handler = WebhookHandler(os.environ['lineSecret'])
 
 # 監聽所有來自 /callback 的 Post Request
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -33,6 +35,28 @@ def callback():
 
     return 'OK'
 
+
+Confirm_template = TemplateSendMessage
+(
+    alt_text='目錄 template',
+    template=ConfirmTemplate(
+        title='ConfirmTemplate',
+        text='了解我的學經歷',
+        actions=[
+            PostbackTemplateAction(
+                label='學歷',
+                text='學歷',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='經歷',
+                text='經歷'
+            )
+        ]
+    )
+)
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # print(event)
@@ -45,21 +69,27 @@ def handle_message(event):
     # input_text = event.message.text.encode('utf-8')
     msg = event.message.text
     if ("name" in msg) or ("Hi" in msg) or ("Hello" in msg) or ("你好" in msg) or ("Yo" in msg):
-        line_bot_api.push_message(event.source.user_id, StickerSendMessage(package_id=11538, sticker_id=51626494))
-        message = TextSendMessage(text="安安你好！\n我是Casper chat bot \n你想知道關於我什麼呢？")
-        replay_message(event,message)
+        line_bot_api.push_message(event.source.user_id, StickerSendMessage(
+            package_id=11538, sticker_id=51626494))
+        message = TextSendMessage(
+            text="安安你好！\n我是Casper chat bot \n你想知道關於我什麼呢？")
+        replay_message(event, message)
     elif ("介紹" in msg) or ("關於我" in msg):
-        message = TextSendMessage(text="我叫做范植承\n生日是84/03/18(雙魚座)\n英文名字是Casper\n個性比較悶騷 喜歡嘗試新東西\n好奇心強 熟了話就比較多")
-        replay_message(event,message)
+        message = TextSendMessage(
+            text="我叫做范植承\n生日是84/03/18(雙魚座)\n英文名字是Casper\n個性比較悶騷 喜歡嘗試新東西\n好奇心強 熟了話就比較多")
+        replay_message(event, message)
     elif ("學歷" in msg) or ("學校" in msg) or ("大學" in msg) or ("研究所" in msg) or ("科系" in msg) or ("學習" in msg):
-        message = TextSendMessage(text="我大學畢業於雲林科技大學 資訊工程系\n研究所畢業於臺北科技大學 資訊工程所\n研究方向是資料處理以及演算法")
-        replay_message(event,message)
+        message = TextSendMessage(
+            text="我大學畢業於雲林科技大學 資訊工程系\n研究所畢業於臺北科技大學 資訊工程所\n研究方向是資料處理以及演算法")
+        replay_message(event, message)
     elif ("經歷" in msg) or ("工作" in msg) or ("實習" in msg) or ("打工" in msg) or ("公司" in msg):
-        message = TextSendMessage(text="我的工作經歷\n2014/07~2014/08\n於葛氏兄弟企業有限公司擔任電腦組裝員\n2018/07~2019/10\n於趨勢科擔任Intern")
-        replay_message(event,message)
+        message = TextSendMessage(
+            text="我的工作經歷\n2014/07~2014/08\n於葛氏兄弟企業有限公司擔任電腦組裝員\n2018/07~2019/10\n於趨勢科擔任Intern")
+        replay_message(event, message)
     elif ("程式" in msg) or ("語言" in msg) or ("證照" in msg):
-        message = TextSendMessage(text="最擅長的程式語言是python\n其他的語言有C/C++\n大學學過一些Java HTML\n多益成績是850")
-        replay_message(event,message)
+        message = TextSendMessage(
+            text="最擅長的程式語言是python\n其他的語言有C/C++\n大學學過一些Java HTML\n多益成績是850")
+        replay_message(event, message)
     # elif ("測試" in msg):
     #     buttons_template = TemplateSendMessage(
     #         alt_text='Buttons Template',
@@ -85,45 +115,25 @@ def handle_message(event):
     #         )
     #     )
     #     line_bot_api.reply_message(event.reply_token, buttons_template)
-    elif ("範例" in msg):     
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='ConfirmTemplate',
-                text='了解我的學經歷',
-                actions=[                              
-                    PostbackTemplateAction(
-                        label='學歷',
-                        text='學歷',
-                        data='action=buy&itemid=1'
-                    ),
-                    MessageTemplateAction(
-                        label='經歷',
-                        text='經歷'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
+    elif ("範例" in msg):
+        line_bot_api.reply_message(event.reply_token, Confirm_template)
 
     else:
         message = TextSendMessage(text=msg)
-        replay_message(event,message)
-    # if input_text == "name":
-    #     message = TextSendMessage(text="My name is Casper")
-    #     replay_message(event,message)
- 
+        replay_message(event, message)
 
 
-def replay_message(event,text):
+def replay_message(event, text):
     line_bot_api.reply_message(
         event.reply_token,
         text)
-        
-def push_message(event,text):
+
+
+def push_message(event, text):
     line_bot_api.push_message(
         event.source.user_id,
-        text)        
+        text)
+
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
@@ -138,11 +148,12 @@ def handle_sticker_message(event):
         line_bot_api.push_message(
             event.source.user_id, StickerSendMessage(package_id=2, sticker_id=173))
 
+
 @app.route('/')
 def index():
     return 'I woke up!!'
 
-     
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
