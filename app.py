5239831a3c1ac20d31ce1d5a97ec532b01be48a9
Casper <aaa40066@gmail.com>
2020-06-22 13:08:58 +0800

    content

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def handle_message(event):
                 ]
             )
         )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
+        line_bot_api.reply_message(event.reply_token, carousel_template)
     
     else:
         message = TextSendMessage(text=msg)
