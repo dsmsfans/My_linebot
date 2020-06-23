dcb0d0e0e3b0cd4e1d9ac7f5b9e1822d4924ee6b
Casper <aaa40066@gmail.com>
2020-06-23 11:08:09 +0800

    add dcard crawler

--- app.py
+++ app.py
@@ -85,6 +85,12 @@ def handle_message(event):
         )
         reply_message(event, buttons_template)
 
+    elif ("dcard" in msg[0:5]):
+        message = TextSendMessage(text=f"開始爬{msg[5:]}版！🥳")
+        reply_message(event, message)
+        dcard_crawl(msg[5:])
+
+
     elif ("測試" in msg):
         carousel_template = TemplateSendMessage(
             alt_text='Carousel Template',
@@ -92,31 +98,31 @@ def handle_message(event):
                 columns=[
                     CarouselColumn(
                         thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg',
-                        title='我的Facebook',
+                        title='My Facebook',
                         actions=[
                             URITemplateAction(
                                 label='Facebook',
-                                uri='https://www.facebook.com/profile.php?id=100001440018890'
+                                uri='https://www.facebook.com/profile.php?id=100001440018890',
                             )
                         ]
                     ),
                     CarouselColumn(
                         thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/5/58/Instagram-Icon.png',
-                        title="我的Instagram",
+                        title="My Instagram",
                         actions=[
                             URITemplateAction(
                                 label="Instagram",
-                                uri='https://www.instagram.com/casper_0318/'
+                                uri='https://www.instagram.com/casper_0318/',
                             )
                         ]
                     ),
                     CarouselColumn(
                         thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg',
-                        title="我的Github",
+                        title="My Github",
                         actions=[
                             URITemplateAction(
                                 label="Github",
-                                uri='https://github.com/dsmsfans'
+                                uri='https://github.com/dsmsfans',
                             )
                         ]
                     )
@@ -124,7 +130,7 @@ def handle_message(event):
             )
         )
         reply_message(event, carousel_template)
-    
+
     else:
         message = TextSendMessage(text=msg)
         reply_message(event, message)
@@ -140,6 +146,50 @@ def push_message(event, text):
     line_bot_api.push_message(
         event.source.user_id,
         text)
+
+
+def dcard_crawl(board):
+    p = requests.Session()
+    url = requests.get(f"https://www.dcard.tw/f/{board}")
+    soup = BeautifulSoup(url.text, "html.parser")
+    sel = soup.select("div.sc-1azsmde-0")
+    a = []
+    for s in sel:
+        a.append(s.find('a').get('href'))
+    url = "https://www.dcard.tw" + a[0]
+    for k in range(0, 10):
+        post_data = {
+            "before": a[-1][6 + len(board):15 + len(board)],
+            "limit": "30",
+            "popular": "true"
+        }
+        r = p.get(f"https://www.dcard.tw/_api/forums/{board}/posts", params=post_data, headers={
+            "Referer": "https://www.dcard.tw/", "User-Agent": "Mozilla/5.0"})
+        data2 = json.loads(r.text)
+        for u in range(len(data2)):
+            Temporary_url = f"/f/{board}/p/" + \
+                str(data2[u]["id"]) + "-" + \
+                str(data2[u]["title"].replace(" ", "-"))
+            a.append(Temporary_url)
+    num = 0
+
+    for url_index, i in enumerate(a):
+        url = "https://www.dcard.tw"+i
+        print(f"Page {url_index}'s URL: {url}")
+        url = requests.get(url)
+        soup = BeautifulSoup(url.text, "html.parser")
+        sel_jpg = soup.find_all('img')
+    for j in sel_jpg:
+        if('https' in j['src']) and num < 10:
+            num += 1
+            print(f"Picture {num} :", j["src"])
+            pic = j['src']
+            push_message(event,ImageSendMessage(original_content_url=pic,preview_image_url=pic))
+            # pic = j['src'].replace(".webp","")
+            # img = requests.get(pic).content
+            # pic_out = open("output/"+str(num)+".jpg", 'wb')
+            # pic_out.write(img)
+            # pic_out.close()
 
 
 @handler.add(MessageEvent, message=StickerMessage)
